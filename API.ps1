7ea0f0cca25e86c1dfdbfcd9075cb32a6c7e174f
Guimill <86968380+Guimill@users.noreply.github.com>
2024-05-14 14:31:43 +0200

    Update API.ps1

--- API.ps1
+++ API.ps1
@@ -1,22 +1,23 @@
-"""
-/***************************************************************************
- API
+######
+#/***************************************************************************
+# API
+#
+#                              -------------------
+#        begin                : 10/04/2024
+#        copyright            : (C) 2023 by Guillaume Milleret
+#        email                : guillaume.milleret@gmail.com
+#	    github		         : https://github.com/Guimill
+# ***************************************************************************/
 
-                              -------------------
-        begin                : 10/04/2024
-        copyright            : (C) 2023 by Guillaume Milleret
-        email                : guillaume.milleret@gmail.com
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-"""
+#/***************************************************************************
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation; either version 2 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# ***************************************************************************/
+#####
 
 
 ######################### API - Automatic Pit Importer ######################################
