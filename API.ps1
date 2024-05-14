efa06f9bf8bf9bb88198b6f020d5aae4e0f62565
Guimill <86968380+Guimill@users.noreply.github.com>
2024-05-14 14:29:50 +0200

    Update API.ps1

--- API.ps1
+++ API.ps1
@@ -1,3 +1,24 @@
+"""
+/***************************************************************************
+ API
+
+                              -------------------
+        begin                : 10/04/2024
+        copyright            : (C) 2023 by Guillaume Milleret
+        email                : guillaume.milleret@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+
 ######################### API - Automatic Pit Importer ######################################
 
 $programFiles = "C:\Program Files"
